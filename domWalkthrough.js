9afc443a74bce8086bfe3d18cda111b174b8ddf7
Jdeeezzy <jdacquel@sea.yearup.com>
2022-05-25 13:23:04 -0700

    dom

---
+++ domWalkthrough.js
@@ -0,0 +1,16 @@
+console.dir(document);
+
+// document.domain will show the IP Address
+console.log(document.domain);
+
+// document.url will show the location of the file 
+console.log(document.URL);
+
+// document.title will call title from the tree
+console.log(document.title)
+
+// document.doctype will show you the type of document
+console.log(document.doctype);
+
+// document.head will show all items in the head
+console.log(document.head);
