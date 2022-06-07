fc0b7529e18080e7ef5c1e4b91622e3c3de265ad
Jdeeezzy <jdacquel@sea.yearup.com>
2022-06-07 13:41:19 -0700

    ifConditions

---
+++ conditionsLIVE.js
@@ -0,0 +1,32 @@
+console.log("Hello World");
+
+var randomNumber = Math.random();
+
+console.log(randomNumber);
+
+// if conditions are met
+// relational operators are used inside of conditonal statements
+if (randomNumber < 0.5){
+    console.log("Hello Justin");
+    console.log(randomNumber);
+}
+if (randomNumber >= 0.5){
+    console.log('Random Number is greater than 0.5');
+    console.log(randomNumber);
+}
+
+// else if statements are statements when conditions are not met
+const nameOfCar = 'Mercedes';
+
+if (nameOfCar === 'Honda') {
+
+    console.log('I drive a Honda');
+
+} else if (nameOfCar === 'BMW'){
+
+    console.log('I drive a BMW');
+
+} else if (nameOfCar === 'Mercedes'){
+
+    console.log('I DRIVE A F***** MERCEDES');
+}
